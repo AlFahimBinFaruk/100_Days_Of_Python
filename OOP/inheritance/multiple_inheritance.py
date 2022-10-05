b0d2a2874784935d92a47864eff896e0f0ac7922
al fahim bin faruk <fmd03700@gmail.com>
2022-10-05 20:15:50 +0600

    updated

--- OOP/inheritance/multiple_inheritance.py
+++ OOP/inheritance/multiple_inheritance.py
@@ -10,6 +10,9 @@ class Dad:
     def hiDad(self):
         print("Hi Dad")
 
+    def greet(self):
+        print("Hi i am his dad.")
+
 
 # Multiple inheritance = one class can inherit multiple class
 class Child(Mom, Dad):
@@ -17,6 +20,8 @@ class Child(Mom, Dad):
     def greet(self):
         print(f"I am Fahim my mom is {self.momName}")
         self.hiMom()
+        # this is also an example of overriding.
+        super().greet()
 
 
 momIn = Mom()
