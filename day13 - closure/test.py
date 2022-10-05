4273185c8d3c386bef827e4372053c926a8f4304
al fahim bin faruk <fmd03700@gmail.com>
2022-10-05 20:01:27 +0600

    updated

--- day13 - closure/test.py
+++ day13 - closure/test.py
@@ -1,11 +1,13 @@
-text="suhan"
+text = "suhan"
 def outerFunction(text):
     text = text
 
     def innerFunction(text):
+        text = text
         print(text)
 
     innerFunction("Zayn")
 
+    print(text)
 
 outerFunction("fahim")
